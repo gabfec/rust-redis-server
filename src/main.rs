240f74a5c524980862e34241af659ff64a709a25
Gabriel Feceoru <gabi.feceoru@gmail.com>
2026-02-06 17:35:33 +0100

    Add handle_connection()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
-use std::io::{Read, Write};
-use std::net::TcpListener;
+use std::io::{Read, Result as IoResult, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,21 +10,26 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
+            Ok(stream) => {
                 println!("accepted new connection");
-                let mut buffer = [0; 512];
-                loop {
-                    let bytes_read = stream.read(&mut buffer);
-                    match bytes_read {
-                        Ok(0) => break,
-                        Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
-                        Err(_) => break,
-                    }
-                }
+                handle_connection(stream).unwrap();
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) -> IoResult<()> {
+    let mut buffer = [0; 1024];
+    loop {
+        let bytes_read = stream.read(&mut buffer);
+        match bytes_read {
+            Ok(0) => break,
+            Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
+            Err(_) => break,
+        }
+    }
+    Ok(())
 }
